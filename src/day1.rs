e7924502b27b9681b5e7687a95bd514d83bb2859
onefangg <pseudopoo@DESKTOP-514V2KP.localdomain>
2024-12-01 16:58:45 +0800

    Day1 check in

---
+++ src/day1.rs
@@ -0,0 +1,60 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+
+pub fn parse_puzzle_input(input_path: &str) -> (Vec<u32>, Vec<u32>) {
+    let content = fs::read_to_string(&Path::new(input_path)).unwrap();
+
+    content
+        .lines()
+        .map(|x| {
+            x.split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect_tuple::<(u32, u32)>()
+                .unwrap()
+        })
+        .unzip()
+}
+
+pub fn solve_part_a(input_path: &str) -> u32 {
+    let (left, right) = parse_puzzle_input(input_path);
+    let sorted_right: Vec<u32> = right.into_iter().sorted().collect();
+    left.iter()
+        .sorted()
+        .enumerate()
+        .map(|(idx, e)| e.abs_diff(sorted_right[idx]))
+        .sum::<u32>()
+}
+
+pub fn solve_part_b(input_path: &str) -> u32 {
+    let (left, right) = parse_puzzle_input(input_path);
+
+    // alternatively let itertools handle it for us
+    // let right_freq = right.into_iter().counts();
+    let right_freq = right.iter().copied().fold(HashMap::new(), |mut acc, e| {
+        acc.entry(e).and_modify(|frq| *frq += 1).or_insert(1);
+        acc
+    });
+    left.iter()
+        .map(|x| x * right_freq.get(x).unwrap_or(&0))
+        .sum::<u32>()
+}
+
+#[cfg(test)]
+mod day1_tests {
+    use super::*;
+
+    #[test]
+    pub fn test_with_sample_data() {
+        assert_eq!(solve_part_a("./inputs/day1_sample.txt"), 11);
+        assert_eq!(solve_part_b("./inputs/day1_sample.txt"), 31);
+    }
+
+    #[test]
+    pub fn test_with_actual_data() {
+        // redacted
+        assert_eq!(solve_part_a("./inputs/day1.txt"), 0);
+        assert_eq!(solve_part_b("./inputs/day1.txt"), 0);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod day1;
+use std::env;
+
+fn main() {
+    let day_selector: u32 = env::args()
+        .nth(1)
+        .expect("Please provide a day number")
+        .parse()
+        .expect("Could not parse day number");
+
+    let problem_selector: char = env::args()
+        .nth(2)
+        .unwrap_or(String::from("a"))
+        .parse()
+        .expect("Could not parse problem selection");
+
+    // match (day_selector, problem_selector) {
+    //     (1, 'a') => day1::solve_part_a(),
+    //     (1, 'b') => day1::solve_part_b(),
+    //     _ => {}
+    // }
+}
