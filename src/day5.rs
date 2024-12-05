b1786dc8e435060348d82eb8bda2180939a79b4e
onefangg <pseudopoo@DESKTOP-514V2KP.localdomain>
2024-12-05 23:17:13 +0800

    Add Day 5 template

---
+++ src/day5.rs
@@ -0,0 +1,30 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+use std::fs;
+use std::path::Path;
+
+pub fn solve_part_a(input_path: &str) -> usize {
+    let content = fs::read_to_string(&Path::new(input_path)).unwrap();
+    0
+}
+
+pub fn solve_part_b(input_path: &str) -> usize {
+    let content = fs::read_to_string(&Path::new(input_path)).unwrap();
+    0
+}
+
+#[cfg(test)]
+mod day5_tests {
+    use super::*;
+    #[test]
+    pub fn test_with_sample_data() {
+        assert_eq!(solve_part_a("./inputs/day5_sample.txt"), 0);
+        assert_eq!(solve_part_b("./inputs/day5b_sample.txt"), 0);
+    }
+
+    #[test]
+    pub fn test_with_actual_data() {
+        assert_eq!(solve_part_a("./inputs/day5.txt"), 0);
+        assert_eq!(solve_part_b("./inputs/day5.txt"), 0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,22 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 use std::env;
 
 fn main() {
-    let day_selector: u32 = env::args()
-        .nth(1)
-        .expect("Please provide a day number")
-        .parse()
-        .expect("Could not parse day number");
-
-    let problem_selector: char = env::args()
-        .nth(2)
-        .unwrap_or(String::from("a"))
-        .parse()
-        .expect("Could not parse problem selection");
+    // let day_selector: u32 = env::args()
+    //     .nth(1)
+    //     .expect("Please provide a day number")
+    //     .parse()
+    //     .expect("Could not parse day number");
+    //
+    // let problem_selector: char = env::args()
+    //     .nth(2)
+    //     .unwrap_or(String::from("a"))
+    //     .parse()
+    //     .expect("Could not parse problem selection");
 
     // match (day_selector, problem_selector) {
     //     (1, 'a') => day1::solve_part_a(),
